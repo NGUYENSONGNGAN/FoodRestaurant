d863433836bda58904edf2786bb29fdffab68ca4
ngan <ngan@s3corp.com.vn>
2022-03-10 09:47:57 +0700

    code update js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
 let menu = document.querySelector("#menubars")
 let navbar = document.querySelector(".navbar")
 let search = document.querySelector("#search-icon")
+let section = document.querySelectorAll("section")
+let navLink = document.querySelectorAll("header .navbar a")
+
+
 menu.onclick = () => {
     menu.classList.toggle('fa-xmark')
     navbar.classList.toggle("active")
 
 }
 window.onscroll = () => {
-    menu.classList.remove('fa-xmark')
-    navbar.classList.remove("active")
+    /* menu.classList.remove('fa-xmark')
+    navbar.classList.remove("active") */
+    section.forEach(sec=>
+        {
+            let top = window.scrollY;
+            let height = sec.offsetHeight;
+            let offset = sec.offsetTop-150;
+            let id = sec.getAttribute('id');
+
+            if(top => offset && top < offset + height )
+            {
+                navLink.forEach(navLinks=>
+                    {
+                        navLinks.classList.remove('active')
+                        document.querySelector("header .navbar a[href*="+id+"]").classList.add('active');
+                    
+                    });
+            };
+        });
 
 }
 search.onclick = () => {
