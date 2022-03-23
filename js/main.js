4a67f9d36c3898ed4bcc1e83336956d41c26f3ff
ngan <ngan@s3corp.com.vn>
2022-03-23 09:09:41 +0700

    FR: write event form oder now

--- js/main.js
+++ js/main.js
@@ -14,12 +14,12 @@ let btn_oder_now = document.querySelectorAll(".home-slider .btn")
     let a = document.querySelector(".content h3").textContent;
     console.log(a)
 } */
-let name_food ='';
-btn_oder_now.forEach(e=>{
-    e.onclick = ()=>{
+let name_food = '';
+btn_oder_now.forEach(e => {
+    e.onclick = () => {
         name_food = document.querySelector(".content h3").textContent;
     }
-  
+
 })
 console.log(name_food);
 
@@ -32,23 +32,20 @@ menu.onclick = () => {
 window.onscroll = () => {
     /* menu.classList.remove('fa-xmark')
     navbar.classList.remove("active") */
-    section.forEach(sec=>
-        {
-            let top = window.scrollY;
-            let height = sec.offsetHeight;
-            let offset = sec.offsetTop-150;
-            let id = sec.getAttribute('id');
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
 
-            if(top >= offset && top < offset + height )
-            {
-                navLink.forEach(navLinks=>
-                    {
-                        navLinks.classList.remove('active')
-                        document.querySelector("header .navbar a[href*="+id+"]").classList.add('active');
-                    
-                    });
-            };
-        });
+        if (top >= offset && top < offset + height) {
+            navLink.forEach(navLinks => {
+                navLinks.classList.remove('active')
+                document.querySelector("header .navbar a[href*=" + id + "]").classList.add('active');
+
+            });
+        };
+    });
 
 }
 search.onclick = () => {
@@ -97,15 +94,15 @@ var swiper = new Swiper(".review-slider", {
 });
 /* ---------------------------jquery------------------ */
 
-$(document).ready(function(){
-    $('.home-slider .content .btn').click(function(){
+$(document).ready(function () {
+    $('.home-slider .content .btn').click(function () {
         var name_food = $(this).prev().prev().text();
         $('#yourOrder').val(name_food);
     });
 })
 
-$(document).ready(function(){
-    $('#order .btn').click(function(){
+$(document).ready(function () {
+    $('#order .btn').click(function () {
         alert('Oder thành công!');
     });
 })
@@ -120,3 +117,65 @@ function FadeOut(){
 
 window.onload = FadeOut();
  */
+
+function Validator(options) {
+    var formElement = document.querySelector(options.form)
+    if (formElement) {
+        options.rules.forEach(function (rule) {
+            var inputElement = formElement.querySelector(rule.selector);
+            var errorElement = inputElement.parentElement.querySelector('.form-message')
+            if (inputElement) {
+                inputElement.onblur = function () {
+                    //value :inputelement.values
+                    //test func:rule.test
+                    var errorMessage = rule.test(inputElement.value);
+                    if (errorMessage) {
+                        errorElement.innerText = errorMessage;
+                        inputElement.classList.add('invalid')
+                    }
+                    else {
+                        errorElement.innerText = '';
+                        inputElement.classList.remove('invalid')
+                    }
+                }
+                inputElement.oninput = function () {
+                   /* 
+                     */
+                    errorElement.innerText = '';
+                    inputElement.classList.remove('invalid')
+                }
+            }
+        })
+    }
+}
+//định nghĩa ruler
+//khi có lỗi trả ra messae lỗi
+//khi hợp lệ => không trả cái gì cra
+Validator.isrequired = function (selector) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.trim() ? undefined : 'vui lòng nhập trường này'
+
+        }
+    }
+}
+Validator.isEmail = function (selector) {
+    return {
+        selector: selector,
+        test: function (value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : 'vui lòng nhập đúng định dạng email'
+
+        }
+    }
+}
+
+
+Validator({
+    form: '#form_orderNow',
+    rules: [
+        Validator.isrequired("#yourName"),
+        Validator.isEmail("#yourOrder")
+    ]
+})
